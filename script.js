1d5c07f015d75544fc24dc607bfddfe2bc7735d6
sushant <patil3093s@gmail.com>
2024-12-26 04:36:08 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const qrText=document.getElementById('qr-text');
+const sizes=document.getElementById('sizes');
+const generateBtn=document.getElementById('generateBtn');
+const downloadBtn=document.getElementById('downloadBtn');
+const qrContainer=document.querySelector('.qr-body')
+
+let size = sizes.value;
+
+generateBtn.addEventListener('click',(e)=>{
+  e.preventDefault();
+  isEmptyInput();
+
+});
+
+ sizes.addEventListener('change',(e)=>{
+  size=e.target.value;
+  isEmptyInput();
+
+ });
+ 
+ downloadBtn.addEventListener('click',()=>{
+  let img=document.querySelector('.qr-body img');
+
+  if(img !==null){
+    let imgAtrr=img.getAttribute('src');
+    downloadBtn.setAttribute("href",imgAtrr);
+
+  }
+  else{
+    downloadBtn.setAttribute("href",`${document.querySelector('canvas').toDataURL}`);
+
+  }
+ });
+
+  function isEmptyInput(){
+    qrText.value.length > 0 ? generateQRCode() : alert("Enter the text URl to genarate qr code");
+
+  
+  }
+  function generateQRCode(){
+    qrContainer.innerHTML="";
+    new QRCode(qrContainer,{
+      text:qrText.value,
+      height:size,
+      width:size,
+      colorLight: "#fff",
+      colorDark: "#000",
+    });
+  }
